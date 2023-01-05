ea5deaa9fa85b4ea605b69438e5f8a4f4b798d85
karlsonru <ivpolovnikov@hotmail.com>
2023-01-05 18:09:51 +0300

    init: setup project

---
+++ dst/server.js
@@ -0,0 +1,5 @@
+import { fstat } from 'fs';
+const a = 'Hello world!';
+console.log(a);
+console.log(fstat);
+export { a };
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { fstat } from 'fs';
+
+const a = 'Hello world!';
+
+console.log(a);
+console.log(fstat);
+
+
+export { a };
